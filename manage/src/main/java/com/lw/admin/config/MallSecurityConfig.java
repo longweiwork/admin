6715f27a1b0e70b0da13dc95c19fa0d3e8617848
LongWei <longweiss521@gmail.com>
2020-12-28 17:08:06 +0800

    manage模块的安全及跨域配置

---
+++ manage/src/main/java/com/lw/admin/config/MallSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.lw.admin.config;
+
+import com.lw.admin.model.UmsResource;
+import com.lw.admin.security.component.DynamicSecurityService;
+import com.lw.admin.security.config.SecurityConfig;
+import com.lw.admin.service.UmsAdminService;
+import com.lw.admin.service.UmsResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * security模块相关配置
+ *
+ * @Author: Brian
+ * @Date: 2020/12/28 15:42
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MallSecurityConfig extends SecurityConfig {
+
+    @Autowired
+    private UmsAdminService adminService;
+    @Autowired
+    private UmsResourceService resourceService;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //获取登录用户信息
+        return username -> adminService.loadUserByUsername(username);
+    }
+
+    @Bean
+    public DynamicSecurityService dynamicSecurityService() {
+        return new DynamicSecurityService() {
+            @Override
+            public Map<String, ConfigAttribute> loadDataSource() {
+                Map<String, ConfigAttribute> map = new ConcurrentHashMap<>();
+                List<UmsResource> resourceList = resourceService.listAll();
+                for (UmsResource resource : resourceList) {
+                    map.put(resource.getUrl(), new org.springframework.security.access.SecurityConfig(resource.getId() + ":" + resource.getName()));
+                }
+                return map;
+            }
+        };
+    }
+}
